f1fd7b567f0c78740f31780a2dfa8c613889a9de
rlaope <piyrw9754@gmail.com>
2026-06-13 12:58:26 +0900

    chore: prepare issue-based PR base

--- test/smoke.test.js
+++
@@ -1,8 +0,0 @@
-import test from "node:test";
-import assert from "node:assert/strict";
-
-import { packageName } from "../src/index.js";
-
-test("exports package identity", () => {
-  assert.equal(packageName, "@rlaope/loop");
-});
